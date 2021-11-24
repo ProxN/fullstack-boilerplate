40826be5e1118781bb7cb3a3f486f6ed538b8776
ProxN <idel.ayoubkan@gmail.com>
2021-11-25 00:24:04 +0100

    chore: add server build script

--- packages/server/@types/express-session/index.d.ts
+++ packages/server/@types/express-session/index.d.ts
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import session from 'express-session';
+
 declare module 'express-session' {
   export interface SessionData {
     userId: string;
